a2dba34c7b14f613a670c341dc61990643dd8a9b
MishManners <mishmanners@github.com>
2020-09-08 15:26:33 +1000

    Update Drawer.vue
    Added animation for div container

--- Lesson 5/src/views/Drawer.vue
+++ Lesson 5/src/views/Drawer.vue
@@ -3,21 +3,40 @@
     <button @click="isOpen = !isOpen">
       My Profile
     </button>
-    <div v-if="isOpen" class="drawer">
-      <img src="../assets/avatar.png" alt="avatar" />
-      <div></div>
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
+
+    <transition>
+      @before-enter="beforeEnter" @enter="enter" @leave="leave" :css="false" >
+
+      <div v-if="isOpen" class="drawer">
+        <img src="../assets/avatar.png" alt="avatar" />
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </transition>
   </div>
 </template>
 
 <script>
+import Velocity from 'velocity-animate'
 export default {
   data() {
     return {
       isOpen: false
+    }
+  },
+  methods: {
+    beforeEnter(el) {
+      el.style.opacity = 0
+      el.style.width = '0em'
+    },
+    enter(el, done) {
+      Velocity(
+        el,
+        { opacity: 1, width: '12em' },
+        { duration: 1000, easing: 'ease0utCubic', complete: done }
+      )
     }
   }
 }
