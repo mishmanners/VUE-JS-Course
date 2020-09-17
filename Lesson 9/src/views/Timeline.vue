aae32dade7e9ccd174265006107a27b33a659b3b
MishManners <mishmanners@github.com>
2020-09-17 14:34:20 +1000

    Add Lesson 9

---
+++ Lesson 9/src/views/Timeline.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <img class="runner first" src="../assets/runner.png" alt="runner" />
+    <img class="runner second" src="../assets/runner.png" alt="runner" />
+    <img class="runner third" src="../assets/runner.png" alt="runner" />
+  </div>
+</template>
+
+<script>
+import gsap from 'gsap'
+
+export default {
+  mounted() {
+    let tl = gsap.timeline({ repeat: -1, repeatDelay: 1 })
+    tl.to('.first', { x: 700, duration: 2, ease: 'expo.out' })
+    tl.to('.second', { x: 700, duration: 2, ease: 'expo.out' }, '<.5')
+    tl.to('.third', { x: 700, duration: 2, ease: 'expo.out' }, '<')
+  }
+}
+</script>
+
+<style scoped>
+.runner {
+  display: block;
+  height: 5em;
+  width: 5em;
+  margin-top: 1.5em;
+}
+</style>
